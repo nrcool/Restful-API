0347463073221b489d14210fe3afa42a678f92c1
NAQVI <nomanalam7214@gmail.com>
2019-09-25 00:20:23 +0200

    added CRUD routes

--- routes/products.js
+++ routes/products.js
@@ -1,28 +1,47 @@
 const express=require("express")
 const router = express.Router();
 const Products=require("../mongooScheme/productsSchema")
-router.get("/",(req,res,next)=>{
-    res.status(200).json({
-        message:"get request for Products"
+router.get("/", (req, res, next) => {
+    Products.find().then(products => {
+        res.send(products)
     })
+    /* res.status(200).json({
+        message:"get request for products"
+    }) */
 })
-router.post("/",(req,res,next)=>{
-    res.status(200).json({
-        message:"post request for Products"
+router.post("/", (req, res, next) => {
+    console.log(req.body)
+    const product = new Products({
+        itemname: req.body.itemname,
+        price: req.body.price,
+        quantity: req.body.quantity
     })
+    product.save()
+     res.status(200).json({
+         postproduct:"Product is Placed"
+     }) 
 })
-router.patch("/:productID",(req,res,next)=>{
-    const id=req.body.productID;
-    res.status(200).json({
-        message:"patch request for orders",
-        id:id
+router.patch("/", (req, res, next) => {
+    products.findByIdAndUpdate(req.body.id,req.body,{new:true},(err, db) => {
+        if (err) {
+            res.status(500).json({
+                message: "error with server"
+            })
+        }else{
+            res.status(200).json({updateproduct:"Product is Updatted"})
+        }
     })
+
 })
-router.delete("/:productID",(req,res,next)=>{
-    const id=req.body.productID;
-    res.status(200).json({
-        message:"delete request for orders",
-        id:id
+router.delete("/", (req, res, next) => {
+    products.findByIdAndDelete(req.body.id,(err, db) => {
+        if (err) {
+            res.status(500).json({
+                message: "error with server"
+            })
+        }else{
+            res.status(200).json({deleteproduct:"Product is Deleted"})
+        }
     })
 })
 
