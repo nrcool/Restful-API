1a5e003d8d2daa9723a4c6ff6b8c0fa7b209efaf
NAQVI <nomanalam7214@gmail.com>
2019-09-24 23:20:36 +0200

    added routes for CRUD

--- routes/orders.js
+++ routes/orders.js
@@ -1,39 +1,48 @@
-const express=require("express")
+const express = require("express")
 const router = express.Router();
-const Orders=require("../mongooScheme/ordersSchema")
+const Orders = require("../mongooScheme/ordersSchema")
 
-router.get("/",(req,res,next)=>{
-    Orders.find().then(orders=>{
+router.get("/", (req, res, next) => {
+    Orders.find().then(orders => {
         res.send(orders)
     })
     /* res.status(200).json({
         message:"get request for orders"
     }) */
 })
-router.post("/",(req,res,next)=>{
-    const order=new Orders({
-        itemname:req.body.itemname,
-        price:req.body.price,
-        quantity:req.body.quantity
+router.post("/", (req, res, next) => {
+    const order = new Orders({
+        itemname: req.body.itemname,
+        price: req.body.price,
+        quantity: req.body.quantity
     })
     order.save()
-   /*  res.status(200).json({
-        message:"post request for orders"
-    }) */
+     res.status(200).json({
+         postorder:"Order is Placed"
+     }) 
 })
-router.patch("/:orderID",(req,res,next)=>{
-    const id=req.body.orderID;
-    res.status(200).json({
-        message:"update request for orders",
-        id:id
+router.patch("/", (req, res, next) => {
+    Orders.findByIdAndUpdate(req.body.id,req.body,{new:true},(err, db) => {
+        if (err) {
+            res.status(500).json({
+                message: "error with server"
+            })
+        }else{
+            res.status(200).json({updateorder:"ORDER is Updatted"})
+        }
     })
+
 })
-router.delete("/:orderID",(req,res,next)=>{
-    const id=req.body.orderID;
-    res.status(200).json({
-        message:"delete request for orders",
-        id:id
+router.delete("/", (req, res, next) => {
+    Orders.findByIdAndDelete(req.body.id,(err, db) => {
+        if (err) {
+            res.status(500).json({
+                message: "error with server"
+            })
+        }else{
+            res.status(200).json({deleteorder:"ORDER is Deleted"})
+        }
     })
 })
 
-module.exports=router
+module.exports = router
