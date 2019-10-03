65f2435dd53148ae4a6fef7c7bc897d2f70c24ce
NAQVI <nomanalam7214@gmail.com>
2019-10-03 17:17:41 +0200

    add username in userSchema

--- mongooScheme/ordersSchema.js
+++ mongooScheme/ordersSchema.js
@@ -2,6 +2,7 @@ const mongoose=require("mongoose");
 
 const orders=mongoose.Schema({
    id:mongoose.Schema.Types.ObjectId,
+   username:String,
    itemname:{type:String,required:true},
    price:{type:String,required:true},
    quantity:{type:String,required:true}
