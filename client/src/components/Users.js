73f0269845bbb75cdf71cb1d0a84effc73b06d38
NAQVI <nomanalam7214@gmail.com>
2019-10-04 00:44:59 +0200

    fetch all users and delete user

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -2,18 +2,59 @@ import React, { Component } from 'react';
 import {connect} from "react-redux";
 
 class Users extends Component {
+    state={
+        users:[],
+        deleteuser:""
+    }
+    getusers=()=>{
+        fetch("/users")
+        .then(res => res.json())
+        .then(res2 => {
+            console.log(res2)
+            this.setState({
+                users: res2
+            })
+        }
+        )
+    }
+    deleteuser=(e)=>{
+        e.preventDefault();
+        const formdata = new FormData(e.target)
+        const searchurl = new URLSearchParams();
+        for (const pair of formdata) {
+            searchurl.append(pair[0], pair[1])
+        }
+        e.target.reset()
+        fetch("/users", { method: "DELETE", body: searchurl })
+            .then(res => res.json())
+                .then(res2 => this.setState({
+                    deleteuser: res2.deleteuser
+                },()=>{
+                    setTimeout(() => {
+                       this.setState({
+                          deleteuser:""
+                       }) 
+                    }, 2000);
+                    
+                }))
+    }
     render() {
         return (<>
             {this.props.data.admin?<div className="userspage">
                 <div className="users">
             <h1>USERS</h1>
                 <div className="getUSERS">
-                <button>Get USERS</button>
-                <p>all USERS</p>
+                <button onClick={this.getusers}>Get Users</button>
+                    <details><summary>all Users </summary>
+                    {this.state.users.length === 0 ? "" : (<>{this.state.users.map(user => {
+                            return (<div key={user._id} style={{ background: "lightgreen", padding: "5px", buser: "2px dotted black" }}><p> <span style={{ background: "lightgray" }}> Id:</span> {user._id} </p>
+                                <p><span style={{ background: "lightgray" }}>  Item Name:</span> {user.username} </p> </div>)
+                        })}</>)}
+                     </details>
                 </div>
             <div className="deleteorder">
                 <p>delete order</p>
-                <form action="/users" method="DELETE">
+                <form action="/users" method="DELETE" onSubmit={this.deleteuser}>
                     <label htmlFor="username"> 
                     <span>   Username: </span>
                       <input type="text" name="username"/>
